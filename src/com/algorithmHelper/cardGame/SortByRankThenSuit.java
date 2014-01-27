dfeed39cb56b0219df133a70e05edbef6df9f4e8
kaibo <haokaibo@outlook.com>
2014-01-27 19:32:40 +0800

    Added CardGame related classes for interface practice.

---
+++ src/com/algorithmHelper/cardGame/SortByRankThenSuit.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.algorithmHelper.cardGame;
+
+import java.util.Comparator;
+
+/**
+ * @author u0149807
+ *
+ */
+public class SortByRankThenSuit implements Comparator<Card> {
+
+	/* (non-Javadoc)
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public int compare(Card o1, Card o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
