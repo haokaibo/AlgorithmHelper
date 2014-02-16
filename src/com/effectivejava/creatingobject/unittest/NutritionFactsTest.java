1c79824bf0c7cb249362b628ed8d7cac272330de
kaibo <haokaibo@outlook.com>
2014-02-16 14:34:29 -0800

    Refined the calling to the builder in the test.

--- src/com/effectivejava/creatingobject/unittest/NutritionFactsTest.java
+++ src/com/effectivejava/creatingobject/unittest/NutritionFactsTest.java
@@ -6,7 +6,6 @@ package com.effectivejava.creatingobject.unittest;
 import org.junit.Test;
 
 import com.effectivejava.creatingobject.NutritionFacts;
-import com.effectivejava.creatingobject.NutritionFacts.Builder;
 
 /**
  * @author Kaibo
@@ -16,9 +15,8 @@ public class NutritionFactsTest {
 
 	@Test
 	public void test() {
-		Builder builder = new Builder(10, 100).calories(3).carbohydrate(2)
-				.fat(1).sodium(4);
-		NutritionFacts nutritionFacts = builder.build();
+		NutritionFacts nutritionFacts = new NutritionFacts.Builder(10, 100)
+				.calories(3).carbohydrate(2).fat(1).sodium(4).build();
 		System.out.println(nutritionFacts.toString());
 	}
 
