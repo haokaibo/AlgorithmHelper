9ade8eb07293126d3727d505604d0cabef2f6c4e
kaibo <haokaibo@outlook.com>
2014-02-22 13:49:08 -0800

    Added the latest implementation way of singleton in Java.

---
+++ src/com/effectivejava/creatingobject/Elivis.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.effectivejava.creatingobject;
+
+/**
+ * @author Kaibo
+ *
+ */
+public enum Elivis {
+	INSTANCE;
+	public static void main(String [] args){
+		Elivis.INSTANCE.getClass();
+	}
+}
