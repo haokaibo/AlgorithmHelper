8766c860fdce22d7aa54d172a39805c55d052199
kaibo <haokaibo@outlook.com>
2014-03-12 23:25:12 -0700

    Added Enum types demo for the "Use EnumMap instead of ordinal indexing".

---
+++ src/com/effectivejava/EnumAnnotations/Herb.java
@@ -0,0 +1,27 @@
+/**
+ * Added demo for the "Use EnumMap instead of ordinal indexing".
+ */
+package com.effectivejava.EnumAnnotations;
+
+/**
+ * @author Kaibo
+ * 
+ */
+public class Herb {
+	public enum Type {
+		ANNUAL, PERENNIAL, BIENNIAL
+	}
+
+	private final String name;
+	public final Type type;
+
+	public Herb(String name, Type type) {
+		this.name = name;
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
---
+++ src/com/effectivejava/EnumAnnotations/unittest/HerbTest.java
@@ -0,0 +1,39 @@
+/**
+ * Added demo for the "Use EnumMap instead of ordinal indexing".
+ */
+package com.effectivejava.EnumAnnotations.unittest;
+
+
+
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.effectivejava.EnumAnnotations.Herb;
+
+/**
+ * @author Kaibo
+ * 
+ */
+public class HerbTest {
+
+	@Test
+	public void test() {
+		// Using ordinal() to index an array - DON'T DO THIS!
+		Herb[] garden = {new Herb("Flower1",Herb.Type.ANNUAL),
+				new Herb("Flower2",Herb.Type.BIENNIAL),
+				new Herb("Flower3",Herb.Type.BIENNIAL)} ;
+		// Using an EnumMap to associate data with an enum
+		Map<Herb.Type, Set<Herb>> herbsByType = new EnumMap<Herb.Type, Set<Herb>>(
+				Herb.Type.class);
+		for (Herb.Type t : Herb.Type.values())
+			herbsByType.put(t, new HashSet<Herb>());
+		for (Herb h : garden)
+			herbsByType.get(h.type).add(h);
+		System.out.println(herbsByType);
+	}
+
+}
