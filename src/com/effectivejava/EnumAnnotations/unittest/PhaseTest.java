0db2a20da8a0f2ca78a20b555facf17578fbea3c
kaibo <haokaibo@outlook.com>
2014-03-12 23:35:00 -0700

    Added unit test for Phase.Transition.

---
+++ src/com/effectivejava/EnumAnnotations/unittest/PhaseTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.effectivejava.EnumAnnotations.unittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.effectivejava.EnumAnnotations.Phase;
+
+/**
+ * @author Kaibo
+ *
+ */
+public class PhaseTest {
+
+	@Test
+	public void test() {
+		assertEquals(Phase.Transition.MELT, Phase.Transition.from(Phase.SOLID, Phase.LIQUID));
+		assertEquals(Phase.Transition.SUBLIME, Phase.Transition.from(Phase.SOLID, Phase.GAS));
+		assertEquals(Phase.Transition.CONDENSE, Phase.Transition.from(Phase.GAS, Phase.LIQUID));
+		assertEquals(Phase.Transition.DEPOSIT, Phase.Transition.from(Phase.GAS, Phase.SOLID));
+		assertEquals(Phase.Transition.BOIL, Phase.Transition.from(Phase.LIQUID, Phase.GAS));
+		assertEquals(Phase.Transition.FREEZE, Phase.Transition.from(Phase.LIQUID, Phase.SOLID));
+	}
+
+}
